f09257ba3cb2a3cb7f27fb03fbd092488b2391f3
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2019-05-21 23:02:25 +0300

    Fixing bug.

--- CrmDeveloperHelper/Helpers/EnumDescriptionTypeConverter.cs
+++ CrmDeveloperHelper/Helpers/EnumDescriptionTypeConverter.cs
@@ -52,7 +52,7 @@ namespace Nav.Common.VSPackages.CrmDeveloperHelper.Helpers
 
                     currentTypeDescriptions.TryAdd(valueString, description);
 
-                    return valueString;
+                    return description;
                 }
 
                 return string.Empty;
