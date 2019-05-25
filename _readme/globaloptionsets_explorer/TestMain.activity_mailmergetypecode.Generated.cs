f8a23b07d2936d2e7f14b8f2d37f14c18d882f7d
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2019-05-25 17:36:43 +0300

    Adding files for wiki.

---
+++ _readme/globaloptionsets_explorer/TestMain.activity_mailmergetypecode.Generated.cs
@@ -0,0 +1,104 @@
+
+namespace Nav.Common.VSPackages.CrmDeveloperHelper.Entities.GlobalOptionSets
+{
+
+    ///<summary>
+    /// DisplayName:
+    ///     (English - United States - 1033): Select an activity type
+    ///     (Russian - 1049): Выберите тип действия
+    /// 
+    /// OptionSet Name: activity_mailmergetypecode      IsCustomOptionSet: True
+    ///</summary>
+    [System.ComponentModel.DescriptionAttribute("Select an activity type")]
+    [System.ComponentModel.TypeConverterAttribute("Nav.Common.VSPackages.CrmDeveloperHelper.Helpers.EnumDescriptionTypeConverter")]
+    public enum activity_mailmergetypecode
+    {
+        ///<summary>
+        /// 4201
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Appointment
+        ///     (Russian - 1049): Встреча
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Appointment")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Appointment_4201 = 4201,
+
+        ///<summary>
+        /// 4202
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Email
+        ///     (Russian - 1049): Электронная почта
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Email")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Email_4202 = 4202,
+
+        ///<summary>
+        /// 4204
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Fax
+        ///     (Russian - 1049): Факс
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Fax")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Fax_4204 = 4204,
+
+        ///<summary>
+        /// 4207
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Letter
+        ///     (Russian - 1049): Письмо
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Letter")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Letter_4207 = 4207,
+
+        ///<summary>
+        /// 4210
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Phone Call
+        ///     (Russian - 1049): Звонок
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Phone Call")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Phone_Call_4210 = 4210,
+
+        ///<summary>
+        /// 42020
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Email via Mail Merge
+        ///     (Russian - 1049): Электронная почта через слияние почты
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Email via Mail Merge")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Email_via_Mail_Merge_42020 = 42020,
+
+        ///<summary>
+        /// 42040
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Fax via Mail Merge
+        ///     (Russian - 1049): Факс через слияние
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Fax via Mail Merge")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Fax_via_Mail_Merge_42040 = 42040,
+
+        ///<summary>
+        /// 42070
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Letter via Mail Merge
+        ///     (Russian - 1049): Письмо через слияние
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Letter via Mail Merge")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Letter_via_Mail_Merge_42070 = 42070,
+    }
+}
---
+++ _readme/globaloptionsets_explorer/TestMain.activity_mailmergetypecode.generated.js
@@ -0,0 +1,15 @@
+if (typeof (GlobalOptionSets) == 'undefined') {
+    GlobalOptionSets = { __namespace: true };
+}
+
+GlobalOptionSets.activity_mailmergetypecodeEnum = {
+
+    'Appointment_4201': { 'value': 4201, 'text': 'Appointment', 'name1033': 'Appointment', 'name1049': 'Встреча' },
+    'Email_4202': { 'value': 4202, 'text': 'Email', 'name1033': 'Email', 'name1049': 'Электронная почта' },
+    'Fax_4204': { 'value': 4204, 'text': 'Fax', 'name1033': 'Fax', 'name1049': 'Факс' },
+    'Letter_4207': { 'value': 4207, 'text': 'Letter', 'name1033': 'Letter', 'name1049': 'Письмо' },
+    'Phone_Call_4210': { 'value': 4210, 'text': 'Phone Call', 'name1033': 'Phone Call', 'name1049': 'Звонок' },
+    'Email_via_Mail_Merge_42020': { 'value': 42020, 'text': 'Email via Mail Merge', 'name1033': 'Email via Mail Merge', 'name1049': 'Электронная почта через слияние почты' },
+    'Fax_via_Mail_Merge_42040': { 'value': 42040, 'text': 'Fax via Mail Merge', 'name1033': 'Fax via Mail Merge', 'name1049': 'Факс через слияние' },
+    'Letter_via_Mail_Merge_42070': { 'value': 42070, 'text': 'Letter via Mail Merge', 'name1033': 'Letter via Mail Merge', 'name1049': 'Письмо через слияние' },
+};
