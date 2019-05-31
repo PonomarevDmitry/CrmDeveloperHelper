a04603f93945bc90c294aaa9a309b4517c6bf4a7
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2019-05-31 23:19:17 +0300

    Adding files for wiki.

---
+++ _readme/csharp_files/gbc_multiselect.Generated.cs
@@ -0,0 +1,64 @@
+
+namespace RFB.Core.GlobalOptionSets
+{
+
+    ///<summary>
+    /// DisplayName:
+    ///     (English - United States - 1033): Multi Select Picklist
+    /// 
+    /// OptionSet Name: gbc_multiselect      IsCustomOptionSet: True
+    /// 
+    /// ComponentType:   Attribute (2)            Count: 2
+    ///     AttributeName                              DisplayName              IsCustomizable    Behavior
+    ///     account.gbc_multiselectpicklist            Multi Select Picklist    True              IncludeSubcomponents
+    ///     gbc_entity_test.gbc_multiselectpicklist    Multi Select Picklist    True              IncludeSubcomponents
+    ///</summary>
+    [System.ComponentModel.DescriptionAttribute("Multi Select Picklist")]
+    [System.ComponentModel.TypeConverterAttribute("Nav.Common.VSPackages.CrmDeveloperHelper.Helpers.EnumDescriptionTypeConverter")]
+    public enum gbc_multiselect
+    {
+        ///<summary>
+        /// 865240000
+        /// DisplayOrder: 1
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): One
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("One")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        One_865240000 = 865240000,
+
+        ///<summary>
+        /// 865240001
+        /// DisplayOrder: 2
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Two
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Two")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Two_865240001 = 865240001,
+
+        ///<summary>
+        /// 865240002
+        /// DisplayOrder: 3
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Three
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Three")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Three_865240002 = 865240002,
+
+        ///<summary>
+        /// 865240003
+        /// DisplayOrder: 4
+        /// 
+        /// DisplayName:
+        ///     (English - United States - 1033): Four
+        ///</summary>
+        [System.ComponentModel.DescriptionAttribute("Four")]
+        [System.Runtime.Serialization.EnumMemberAttribute()]
+        Four_865240003 = 865240003,
+    }
+}
