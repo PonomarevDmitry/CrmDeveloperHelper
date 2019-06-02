f3aa84b599db4d3572ac1e2c6e8ae335a24b6662
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2019-06-02 14:30:56 +0300

    Adding files for wiki.

---
+++ _readme/javascript_files_generating/workflow_runas.generated.js
@@ -0,0 +1,12 @@
+if (typeof (GlobalOptionSets) == 'undefined') {
+    GlobalOptionSets = { __namespace: true };
+}
+
+// ComponentType:   Attribute (2)            Count: 1
+//     AttributeName     DisplayName    IsCustomizable    Behavior
+//     workflow.runas    Run As User    True              IncludeSubcomponents
+GlobalOptionSets.workflow_runasEnum = {
+
+    'Owner_0': { 'value': 0, 'text': 'Owner', 'displayOrder': 1, 'name1033': 'Owner', 'name1049': 'Ответственный' },
+    'Calling_User_1': { 'value': 1, 'text': 'Calling User', 'displayOrder': 2, 'name1033': 'Calling User', 'name1049': 'Вызывающий пользователь' },
+};
