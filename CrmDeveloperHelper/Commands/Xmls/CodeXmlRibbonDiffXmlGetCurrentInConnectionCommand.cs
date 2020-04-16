9b48d65699359d91bee2c4f6712087ea17366150
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2020-04-16 18:40:14 +0300

    Improving GUI.

--- CrmDeveloperHelper/Commands/Xmls/CodeXmlRibbonDiffXmlGetCurrentInConnectionCommand.cs
+++ CrmDeveloperHelper/Commands/Xmls/CodeXmlRibbonDiffXmlGetCurrentInConnectionCommand.cs
@@ -38,6 +38,7 @@ namespace Nav.Common.VSPackages.CrmDeveloperHelper.Commands.Xmls
                 , Intellisense.Model.IntellisenseContext.IntellisenseContextAttributeEntityName
                 , out var attribute
                 , AbstractDynamicCommandXsdSchemas.RootRibbonDiffXml
+                , AbstractDynamicCommandXsdSchemas.RootRibbonDefinitions
             );
 
             if (attribute != null)
