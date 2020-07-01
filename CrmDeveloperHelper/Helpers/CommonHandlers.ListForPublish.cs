06aac9412c0f439af69a411931a0eac752ec284e
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2020-07-01 20:11:07 +0300

    Refactoring.

---
+++ CrmDeveloperHelper/Helpers/CommonHandlers.ListForPublish.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Linq;
+
+namespace Nav.Common.VSPackages.CrmDeveloperHelper.Helpers
+{
+    public static partial class CommonHandlers
+    {
+        private static bool CheckInPublishListSingle(EnvDTE80.DTE2 applicationObject, Func<string, bool> checker)
+        {
+            bool result = false;
+
+            if (applicationObject != null)
+            {
+                try
+                {
+                    var helper = DTEHelper.Create(applicationObject);
+
+                    var selectedFiles = helper.GetSelectedFilesFromListForPublish();
+
+                    var count = selectedFiles.Where(f => checker(f.FilePath)).Take(2).Count();
+
+                    result = count == 1;
+                }
+                catch (Exception ex)
+                {
+                    DTEHelper.WriteExceptionToOutput(null, ex);
+
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        private static bool CheckInPublishListAny(EnvDTE80.DTE2 applicationObject, Func<string, bool> checker)
+        {
+            bool result = false;
+
+            if (applicationObject != null)
+            {
+                try
+                {
+                    var helper = DTEHelper.Create(applicationObject);
+
+                    var selectedFiles = helper.GetSelectedFilesFromListForPublish();
+
+                    result = selectedFiles.Any(f => checker(f.FilePath));
+                }
+                catch (Exception ex)
+                {
+                    DTEHelper.WriteExceptionToOutput(null, ex);
+
+                    result = false;
+                }
+            }
+
+            return result;
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceTextSingle(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceTextSingle), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceTextSingleInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceTextSingleInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListSingle(applicationObject, FileOperations.SupportsWebResourceTextType);
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceTextAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceTextAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceTextAnyInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceTextAnyInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListAny(applicationObject, FileOperations.SupportsWebResourceTextType);
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceJavaScriptSingle(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceJavaScriptSingle), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceJavaScriptSingleInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceJavaScriptSingleInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListSingle(applicationObject, FileOperations.SupportsJavaScriptType);
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAnyInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAnyInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListAny(applicationObject, FileOperations.SupportsJavaScriptType);
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceSingle(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceSingle), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceSingleInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceSingleInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListSingle(applicationObject, FileOperations.SupportsWebResourceType);
+        }
+
+        internal static void ActionBeforeQueryStatusListForPublishWebResourceAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
+        {
+            if (!menuCommand.Enabled && !menuCommand.Visible)
+            {
+                return;
+            }
+
+            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceAnyInternal);
+
+            if (visible == false)
+            {
+                menuCommand.Enabled = menuCommand.Visible = false;
+            }
+        }
+
+        private static bool ActionBeforeQueryStatusListForPublishWebResourceAnyInternal(EnvDTE80.DTE2 applicationObject)
+        {
+            return CheckInPublishListAny(applicationObject, FileOperations.SupportsWebResourceType);
+        }
+    }
+}
--- CrmDeveloperHelper/Helpers/CommonHandlers.cs
+++ CrmDeveloperHelper/Helpers/CommonHandlers.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualStudio.Shell;
 using Nav.Common.VSPackages.CrmDeveloperHelper.Model;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Caching;
 using System.Text.RegularExpressions;
@@ -9,7 +8,7 @@ using System.Xml.Linq;
 
 namespace Nav.Common.VSPackages.CrmDeveloperHelper.Helpers
 {
-    public static class CommonHandlers
+    public static partial class CommonHandlers
     {
         private static readonly TimeSpan _cacheItemSpan = TimeSpan.FromSeconds(1);
 
@@ -1545,158 +1544,6 @@ namespace Nav.Common.VSPackages.CrmDeveloperHelper.Helpers
             }
 
             return false;
-        }
-
-        private static bool CheckInPublishListSingle(EnvDTE80.DTE2 applicationObject, Func<string, bool> checker)
-        {
-            bool result = false;
-
-            if (applicationObject != null)
-            {
-                try
-                {
-                    var helper = DTEHelper.Create(applicationObject);
-
-                    var selectedFiles = helper.GetSelectedFilesFromListForPublish();
-
-                    var count = selectedFiles.Where(f => checker(f.FilePath)).Take(2).Count();
-
-                    result = count == 1;
-                }
-                catch (Exception ex)
-                {
-                    DTEHelper.WriteExceptionToOutput(null, ex);
-
-                    result = false;
-                }
-            }
-
-            return result;
-        }
-
-        private static bool CheckInPublishListAny(EnvDTE80.DTE2 applicationObject, Func<string, bool> checker)
-        {
-            bool result = false;
-
-            if (applicationObject != null)
-            {
-                try
-                {
-                    var helper = DTEHelper.Create(applicationObject);
-
-                    var selectedFiles = helper.GetSelectedFilesFromListForPublish();
-
-                    result = selectedFiles.Any(f => checker(f.FilePath));
-                }
-                catch (Exception ex)
-                {
-                    DTEHelper.WriteExceptionToOutput(null, ex);
-
-                    result = false;
-                }
-            }
-
-            return result;
-        }
-
-        internal static void ActionBeforeQueryStatusListForPublishWebResourceTextSingle(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
-        {
-            if (!menuCommand.Enabled && !menuCommand.Visible)
-            {
-                return;
-            }
-
-            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceTextSingle), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceTextSingleInternal);
-
-            if (visible == false)
-            {
-                menuCommand.Enabled = menuCommand.Visible = false;
-            }
-        }
-
-        private static bool ActionBeforeQueryStatusListForPublishWebResourceTextSingleInternal(EnvDTE80.DTE2 applicationObject)
-        {
-            return CheckInPublishListSingle(applicationObject, FileOperations.SupportsWebResourceTextType);
-        }
-
-        internal static void ActionBeforeQueryStatusListForPublishWebResourceTextAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
-        {
-            if (!menuCommand.Enabled && !menuCommand.Visible)
-            {
-                return;
-            }
-
-            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceTextAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceTextAnyInternal);
-
-            if (visible == false)
-            {
-                menuCommand.Enabled = menuCommand.Visible = false;
-            }
-        }
-
-        private static bool ActionBeforeQueryStatusListForPublishWebResourceTextAnyInternal(EnvDTE80.DTE2 applicationObject)
-        {
-            return CheckInPublishListAny(applicationObject, FileOperations.SupportsWebResourceTextType);
-        }
-
-        internal static void ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
-        {
-            if (!menuCommand.Enabled && !menuCommand.Visible)
-            {
-                return;
-            }
-
-            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAnyInternal);
-
-            if (visible == false)
-            {
-                menuCommand.Enabled = menuCommand.Visible = false;
-            }
-        }
-
-        private static bool ActionBeforeQueryStatusListForPublishWebResourceJavaScriptAnyInternal(EnvDTE80.DTE2 applicationObject)
-        {
-            return CheckInPublishListAny(applicationObject, FileOperations.SupportsJavaScriptType);
-        }
-
-        internal static void ActionBeforeQueryStatusListForPublishWebResourceSingle(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
-        {
-            if (!menuCommand.Enabled && !menuCommand.Visible)
-            {
-                return;
-            }
-
-            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceSingle), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceSingleInternal);
-
-            if (visible == false)
-            {
-                menuCommand.Enabled = menuCommand.Visible = false;
-            }
-        }
-
-        private static bool ActionBeforeQueryStatusListForPublishWebResourceSingleInternal(EnvDTE80.DTE2 applicationObject)
-        {
-            return CheckInPublishListSingle(applicationObject, FileOperations.SupportsWebResourceTextType);
-        }
-
-        internal static void ActionBeforeQueryStatusListForPublishWebResourceAny(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
-        {
-            if (!menuCommand.Enabled && !menuCommand.Visible)
-            {
-                return;
-            }
-
-            bool visible = CacheValue(nameof(ActionBeforeQueryStatusListForPublishWebResourceAny), applicationObject, ActionBeforeQueryStatusListForPublishWebResourceAnyInternal);
-
-            if (visible == false)
-            {
-                menuCommand.Enabled = menuCommand.Visible = false;
-            }
-        }
-
-        private static bool ActionBeforeQueryStatusListForPublishWebResourceAnyInternal(EnvDTE80.DTE2 applicationObject)
-        {
-            return CheckInPublishListAny(applicationObject, FileOperations.SupportsWebResourceType);
         }
 
         internal static void ActionBeforeQueryStatusFilesToAdd(EnvDTE80.DTE2 applicationObject, OleMenuCommand menuCommand)
