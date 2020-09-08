b7f8b902924e3956e2d0119b4274266fa282bb36
Ponomarev Dmitry <dmitriy.ponomarev@glowbyteconsulting.com>
2020-09-08 23:11:53 +0300

    Fixing bug.

--- CrmDeveloperHelper/Views/WindowTreePlugin.xaml.cs
+++ CrmDeveloperHelper/Views/WindowTreePlugin.xaml.cs
@@ -3459,7 +3459,7 @@ namespace Nav.Common.VSPackages.CrmDeveloperHelper.Views
                 return;
             }
 
-            await AddPluginTypeStepsToSolution(nodeItem.PluginAssemblyId.Value, true, null);
+            await AddAssemblyToSolution(nodeItem.PluginAssemblyId.Value, true, null);
         }
 
         private async void AddAssemblyToCrmSolutionLast_Click(object sender, RoutedEventArgs e)
